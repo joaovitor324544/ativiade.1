0732cecfd41e3e308665331ed279b6b25177ba6a
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-04-16 11:25:15 +0000

    save

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
     botaoDeAcessibilidade.addEventListener('click', function(){
-        botaoDeAcessibilidade.classList.toggle('rotacoa-botao')
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
         opcoesAcessibilidade.classList.toggle('apresenta-lista')
 
     })
