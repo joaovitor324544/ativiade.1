2e311ac89d384343e8ce9ce3f76d72c6ccf7d7a1
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-04-23 11:43:18 +0000

    savejts

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+
     const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    botaoDeAcessibilidade.addEventListener('click', function(){
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao')
         opcoesAcessibilidade.classList.toggle('apresenta-lista')
 
     })
+    
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+    
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
